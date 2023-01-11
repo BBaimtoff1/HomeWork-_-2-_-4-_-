c332e831ced4b4062e0557cbad07c21c4d545888
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-11 12:23:29 +0300

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+
+    }
+    public void deposit(double sum){
+        amount = amount + sum;
+    }
+    public void withDraw(int sum) throws LimitException{
+        if (sum < amount){
+            throw new LimitException("У вас недостаточна средсв для снятий суммы", amount );
+
+        } else {
+      amount = amount - sum;
+            System.out.println("Вы сняли деньги в размере" + sum);
+        }
+
+    }
 }
